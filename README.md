17b49ad7759c48e2d29dae28e9e196c0a836b348
rubenwest <rubensg90@gmail.com>
2024-03-26 13:25:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mt-test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mt-test)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+}
+// ApiKey: 57ToUKR3yqDwPKpD3M7Tfnx8QFZ8cAUY
